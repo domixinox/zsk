2285f2da1c3867265a321abb1b4f67edac91e022
Jakub Kupczyk <69753899+jkbkupczyk@users.noreply.github.com>
2020-09-23 14:18:59 +0200

    Added phonebook and BST

--- JS/AplikacjeMobilne/javascript - labolatoria 3.js
+++ JS/AplikacjeMobilne/javascript - labolatoria 3.js
@@ -2,7 +2,7 @@
 
 
 // Task 1 - Area of any polygon
-class Vertice{
+class Vertice {
     constructor(x, y) {
         this.x = x;
         this.y = y;
@@ -22,7 +22,7 @@ class Polygon {
         this.vertices = vertices;
     }
 
-    polyArea() {
+    polyArea(){
         let num = 0;
 
         for(let i = 0; i < this.vertices.length - 1; i++){
@@ -41,26 +41,110 @@ let verticesArr = [
     new Vertice(4, 10)
 ];
 
-let poly1 = new Polygon(verticesArr);
+let squareArr = [
+    new Vertice(1, 1),
+    new Vertice(0, 3),
+    new Vertice(3, 0),
+    new Vertice(3, 3)
+];
 
-console.log(poly1.polyArea()); // Output: 45.5
+let poly = new Polygon(verticesArr);
+let square = new Polygon(squareArr);
+
+console.log("poly area: ", poly.polyArea(), "\nsquare Area: ", square.polyArea()); // Output: 45.5 and 1.5
 
 
 
 // Task 2 - BST Tree
-class Node{
-    constructor(data) {
+class Node {
+    constructor(value){
         this.left = null;
         this.right = null;
-        this.data = data;
+        this.value = value;
     }
 }
 
+class Tree {
+    constructor(){
+        this.root = null;
+    }
 
+    createTree(nodes){
+        for (let i = 0; i < nodes.length; i++) {
+            if(this.root == null){
+                this.root = nodes[i];
+            }
+            else {
+                this.insertNode(this.root, nodes[i]);
+            }
+        }
+    }
 
+    insertNode(root, node){
+        if(node.value < root.value){
+            if(root.left == null){
+                root.left = node;
+            }
+            else{ this.createTree(root.left, node); }
+        }
+        else{
+            if(root.right == null){
+                root.right = node;
+            }
+            else { this.createTree(root.right, node); }
+        }
+    }
+}
 
+let nodes = [
+    new Node(100),
+    new Node(20),
+    new Node(12),
+    new Node(16),
+    new Node(77),
+    new Node(56),
+    new Node(67)
+];
 
+let tree = new Tree();
+tree.createTree(nodes);
 
+// Task 3 - Person phonebook
+class Person {
+    constructor(name, surName, address, birthDate, birthPlace){
+        this.name = name;
+        this.surName = surName;
+        this.address = address;
+        this.birthDate = birthDate;
+        this.birthPlace = birthPlace;
+    }
+}
 
+function match(people, criteria){
+    if(people.name == criteria || people.surName == criteria || people.address == criteria || people.birthDate == criteria || people.birthDate == criteria) {
+        return true;
+    }
+    return false;
+}
 
-// Task 3 - Person...
+function findPerson(people, ...criteria){
+    let tab = [];
+
+    for (let i = 0; i < people.length; i++){
+        for(let j = 0; j < criteria.length; j++){
+            if(match(people[i], criteria[j])){ 
+                tab.push(people[i]);
+            }
+        }
+    }
+    return tab;
+}
+
+let people = [
+    new Person("Jakub", "Kupczyk", "Plewiska", "01-06-2001", "Poznan"),
+    new Person("Jakub", "Kowalski", "Konin", "22-22-2222", "Konin"),
+    new Person("Ala", "Makota", "Konin", "11-11-1111", "Konin")
+];
+
+console.table(findPerson(people, "Jakub")); // Output: "Jakub Kupczyk" from "Poznan" and "Jakub Kowalski" from "Konin"
+console.table(findPerson(people, "11-11-1111")); // Output: "Ala Makota"
